c6de01cd7be35225ddc1ce02fad65521961afd6d
Morasiu <morasiu2@gmail.com>
2025-07-28 09:33:02 +0200

    Adds debug console and logging infrastructure
    Implements a debug console page with logging to assist in development and debugging.
    
    It introduces a `DebugLoggerProvider` to capture log messages and display them in the console.
    It also includes a button to clear the logs.
    
    Minor CSS adjustments were made to improve the layout and appearance of various components.

--- AnimeVideoDownloader/BlazorComponents/Extensions/ServiceCollectionExtensions.cs
+++ AnimeVideoDownloader/BlazorComponents/Extensions/ServiceCollectionExtensions.cs
@@ -1,15 +1,43 @@
 ﻿using BlazorComponents.Components.Anime.Services;
+using BlazorComponents.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace BlazorComponents.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        // Track whether the debug logger has been registered
+        private static bool _debugLoggerRegistered = false;
+
+        // Store a singleton instance of the provider
+        private static DebugLoggerProvider? _debugLoggerProvider = null;
+
         public static IServiceCollection AddBlazorComponentsServices(this IServiceCollection services)
         {
             // Register component services
             services.AddSingleton<IAnimeService, AnimeService>();
+            services.AddLogging(builder => {
+                builder.SetMinimumLevel(LogLevel.Information);
 
+                // Register the DebugLoggerProvider with the logging system
+                if (!_debugLoggerRegistered)
+                {
+                    builder.Services.AddSingleton<DebugLoggerProvider>(sp => {
+                        if (_debugLoggerProvider == null)
+                        {
+                            _debugLoggerProvider = new DebugLoggerProvider((level, category, message) => {
+                                // This will be set later by the Debug component
+                                DebugLoggerProvider.LogMessageAction?.Invoke(level, category, message);
+                            });
+                        }
+                        return _debugLoggerProvider;
+                    });
+
+                    _debugLoggerRegistered = true;
+                }
+            });
+            
             return services;
         }
     }
