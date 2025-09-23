1749de936a0a540602c6fb2e15fc7dc02cff1179
Morasiu <morasiu2@gmail.com>
2025-09-23 13:51:45 +0200

    Adds SourcesUpdatedAt property to Episodes
    Adds a SourcesUpdatedAt property to the Episodes entity to track when the sources for an episode were last updated.
    This allows for better management of source updates and prevents unnecessary re-fetching of sources.

--- AnimeVideoDownloader/BlazorComponents/Migrations/20250923115037_Sources updated at.Designer.cs
+++ AnimeVideoDownloader/BlazorComponents/Migrations/20250923115037_Sources updated at.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlazorComponents.Services.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorComponents.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250923115037_Sources updated at")]
+    partial class Sourcesupdatedat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AnimeVideoDownloader/BlazorComponents/Migrations/20250923115037_Sources updated at.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorComponents.Migrations
+{
+    /// <inheritdoc />
+    public partial class Sourcesupdatedat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SourcesUpdatedAt",
+                table: "Episodes",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SourcesUpdatedAt",
+                table: "Episodes");
+        }
+    }
+}
